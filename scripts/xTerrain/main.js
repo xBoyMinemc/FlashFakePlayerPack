2993d9299671020cf2c6bca9b31929179d72970b
xBoyMinemc <86919167+xBoyMinemc@users.noreply.github.com>
2022-10-31 15:04:28 +0000

    Add files via upload

--- scripts/xTerrain/main.js
+++ scripts/xTerrain/main.js
@@ -1,14 +1,13 @@
-;;"请注意，这是一份公测版，仅供正式版版提供完善意见所用，生命周期约为7~14w";;
+;;"请注意，这是一份正式版，预估生命周期约为1~2m";;
 ;;"一切权力归云梦所有";;
 ;;"https://github.com/xboyMinemc/FUCKFakePlayerPack";;
 import * as GameTest from "@minecraft/server-gametest";
 import { 
 	world,
   Location,
-  Entity,
   BlockLocation,
-  EntityQueryOptions,
-  EntityRaycastOptions,
+  // EntityQueryOptions,
+  // EntityRaycastOptions,
   BlockRaycastOptions
   } from "@minecraft/server";
   
@@ -20,10 +19,12 @@ const xboySimCmdHead = "假人";                      ;;" 命令头 ";;
 const 挖掘标识符 = "挖掘标识符";
 const 攻击标识符 = "攻击标识符";
 const 自动攻击标识符 = "自动攻击标识符";
+const 跳跃标识符 = "跳跃标识符";
 const ture = true;
 const 主世界 = world.getDimension("overworld");
 const nether = world.getDimension("nether");
 const the_end = world.getDimension("the end");
+const mojang = {};
 
 let xboyMinemcSIMlist = {};
 let cmd = function(who,what,cmd_String){
@@ -46,8 +47,8 @@ let cmd_ = function(where,what,cmd_String){
     }
 };
 
-let 工具人们 = [];
-let tickWaitTimes = 20*60*60*24*365
+let 工具人们 = [];;;;;;;;;;;;;;;;;;;;;
+let tickWaitTimes = 20*60*60*24*365;;
 let xboyTestsList = {};;;;;;;;;;;;;;;
 let xboyTooleesList = {};;;;;;;;;;;;;
 try {
@@ -68,17 +69,17 @@ try {
           let 工具人 = test.spawnSimulatedPlayer(new BlockLocation(0,2,0), `工具人-`);            ;;"xboy";;
               工具人.addTag(xboySign);;
               // 工具人.breakBlock(new BlockLocation(0,1,1))
-          let x = (工具人.location.x-0.5).toFixed(0);
-          let y =  工具人.location.y.toFixed(0);
-          let z = (工具人.location.z-0.5).toFixed(0); //for blockLocation
+          let x = (工具人.location.x-0.5)>>0;
+          let y =  工具人.location.y>>0;
+          let z = (工具人.location.z-0.5)>>0; //for blockLocation
               工具人.addTag("#xyz#"+x+"#"+(y-2)+"#"+z);;
               xboyTestsList[工具人.name] = test;
               xboyTooleesList[工具人.name] = 工具人
-              工具人.breakBlock(new BlockLocation(+x,y-1,+z))
+              工具人.breakBlock(new BlockLocation(+x,y-1,z))
               工具人们.push(工具人);;;;;
-              工具人.runCommandAsync('gamerule domobspawning true');    "凑活解决刷怪问题"
-              工具人.runCommandAsync('gamerule dodaylightcycle true');  "凑活解决时间问题"
-              工具人.runCommandAsync('gamerule randomtickspeed 1');     "凑活解决tick问题"
+              工具人.runCommandAsync('gamerule domobspawning true');    ;;;"凑活解决刷怪问题";;;
+              工具人.runCommandAsync('gamerule dodaylightcycle true');  ;;;"凑活解决时间问题";;;
+              工具人.runCommandAsync('gamerule randomtickspeed 1');     ;;;"凑活解决tick问题";;;
 
     } })
       .maxTicks(tickWaitTimes)
@@ -90,7 +91,7 @@ try {
   }
 
 } catch (err) {
-  主世界.runCommandAsync(`me ${err}`)
+  主世界.runCommandAsync(`me Core-Dump ${err}`)
 }
 
       function 获取眼前的假人实体(逻辑主体,距离){
@@ -109,10 +110,10 @@ try {
       };
       function 获取附近的非玩家实体(逻辑主体,距离){
         let 吃个桃桃 = {}//new EntityQueryOptions();
-            吃个桃桃.maxDistance = 距离;                                                               ;;"距离";;
-            吃个桃桃.location    = 逻辑主体.location;                                                  ;;"中心坐标";;
-            吃个桃桃.excludeTypes= ["minecraft:player","minecraft:arrow","minecraft:xp_orb","minecraft:item"];                                              ;;"中心坐标";;
-            吃个桃桃.dimension   = 逻辑主体.dimension;
+            吃个桃桃.maxDistance = 8;                                                               ;;"距离";;
+            吃个桃桃.location    = new Location(逻辑主体.location.x,逻辑主体.location.y,逻辑主体.location.z);                                                  ;;"中心坐标-ri泥god";;
+            吃个桃桃.excludeTypes= ["minecraft:player","minecraft:arrow","minecraft:xp_orb","minecraft:item"];                                              ;;"排除掉的实体类型";;
+            吃个桃桃.closest   = 1;
         let 实体们 = 逻辑主体.dimension.getEntities(吃个桃桃);
         let 实体组 = [];
           for(let 实体 of 实体们)实体组.push(实体);;;;;;;;;;;;
@@ -131,7 +132,7 @@ try {
         let x = 眼前的方块绝对坐标.x - 源坐标[0];
         let y = 眼前的方块绝对坐标.y - 源坐标[1];
         let z = 眼前的方块绝对坐标.z - 源坐标[2];
-        主世界.runCommandAsync(`me ${源坐标.join("#")} x:${眼前的方块绝对坐标.x} y:${眼前的方块绝对坐标.y} z:${眼前的方块绝对坐标.z} ### ${x+'#'+y+'#'+z+'#'}`)
+        // 主世界.runCommandAsync(`me ${源坐标.join("#")} x:${眼前的方块绝对坐标.x} y:${眼前的方块绝对坐标.y} z:${眼前的方块绝对坐标.z} ### ${x+'#'+y+'#'+z+'#'}`)
         return new BlockLocation(x,y,z);;"不绝对咯";;
       };
 
@@ -203,16 +204,19 @@ if(!工具人们.length)return;
       }catch(e){};;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
       try{
                   if(工具人.hasTag(挖掘标识符))工具人.breakBlock(获取假人实体眼前的方块的相对坐标(工具人));
-      }catch(e){//  主世界.runCommandAsync(`me ${e}`)
+      }catch(e){  主世界.runCommandAsync(`me ${e}`)
       };;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
       try{
                   if(工具人.hasTag(攻击标识符))工具人.attackEntity(获取眼前的实体(工具人,4));
       }catch(e){};;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
       try{
-                  if(工具人.hasTag(自动攻击标识符))工具人.lookAtEntity(获取附近的非玩家实体(工具人,4)[0])
-      }catch(e){};;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+                  if(工具人.hasTag(自动攻击标识符))工具人.lookAtEntity(获取附近的非玩家实体(工具人,4)[0]);
+      }catch(e){工具人.runCommandAsync("me "+e)};;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
       try{
                   if(工具人.hasTag(自动攻击标识符) && 获取眼前的实体(工具人,4).name!="")工具人.attackEntity(获取眼前的实体(工具人,4));
+      }catch(e){};;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+      try{
+                  if(工具人.hasTag(跳跃标识符))工具人.jump();
       }catch(e){};;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     })
 
@@ -251,7 +255,7 @@ world.events.beforeChat.subscribe( M_event => {
     
     const {message,sender} = M_event;
     const 发起者 = sender;
-    var   消息   = message;
+    let   消息   = message;
     let x = +(发起者.location.x-0.5).toFixed(0);
     let y = + 发起者.location.y.toFixed(0);
     let z = +(发起者.location.z-0.5).toFixed(0); //for blockLocation
@@ -259,13 +263,13 @@ world.events.beforeChat.subscribe( M_event => {
       // 发起者.dimension.spawnEntity("minecraft:player",new BlockLocation(x,y,z));//另一个愚蠢的想法
 
       // let sim = new Entity()
-
       
       
     
       // if(消息=="end"){发起者.teleport(发起者.location,the_end,0,0)}
       // if(消息=="nether"){发起者.teleport(发起者.location,nether,0,0)} 
             if(消息.startsWith(xboySimCmdHead)==(true==false)) return;
+            const 眼前的工具人 = 获取眼前的假人实体(发起者, 16);
                消息 = 消息.replace(xboySimCmdHead,"");
             if(消息== "创建"){
               M_event.cancel=ture;
@@ -301,8 +305,8 @@ world.events.beforeChat.subscribe( M_event => {
                                             ;;
                                            };;
                 
-                发起者.runCommandAsync( `structure save ${name}_1 ${x-1} ${__y+1} ${z-1} ${x+1} ${__y+1} ${z-2} disk`);  ;;"保存原地形";;
-                发起者.runCommandAsync( `structure save ${name}_0 ${x-3} ${__y-2} ${z-3} ${x} ${__y} ${z} disk`);
+                发起者.runCommandAsync( `structure save ${name}_1 ${x-1} ${__y+1} ${z-1} ${x+1} ${__y+1} ${z-2} false memory true`);  ;;"保存原地形";;
+                发起者.runCommandAsync( `structure save ${name}_0 ${x-3} ${__y-2} ${z-3} ${x} ${__y} ${z} false memory true`);
                 
                 cmd_(发起者.dimension, name, `fill ${x-1} ${__y-3} ${z-1} ${x+1} ${__y+1} ${z+1} air 0 replace structure_block`);
                 // cmd_(发起者.dimension, name, `setblock ${x} ${__y+1} ${z} air 0`);
@@ -310,7 +314,7 @@ world.events.beforeChat.subscribe( M_event => {
                 cmd_(发起者.dimension, name, `structure load ${name}_0 ${x-3} ${__y-2} ${z-3}`);
                   
                 cmd( 发起者, name, `execute @s ${x} ${__y+1} ${z-3} gametest run 假人行为:${name}`);;;;;;;;;;;;;;;;;;;;;;;"这里缺个令牌桶，限制创建速度，降低高频率下单一临时变量导致的冲突问题可能性";;;;;;;;;;;;;;;;;;;;;;;
-                
+                ;;;;"什么？execute更新了？什么？跟实验玩法开关有关？什么？要适配？你要相信生命会自己找到出路";;;
                 cmd( 发起者, name, `execute as @s positioned ${x} ${__y+1} ${z-3} run gametest run 假人行为:${name}`);;;;;;;;;;;;;;;;;;;;;;;"这里缺个令牌桶，限制创建速度，降低高频率下单一临时变量导致的冲突问题可能性";;;;;;;;;;;;;;;;;;;;;;;
                 
                 
@@ -326,23 +330,6 @@ world.events.beforeChat.subscribe( M_event => {
 
               for(let i in 工具人们)if(工具人们[i])发起者.runCommandAsync(`tellraw @s {"rawtext":[{"text":"§e§l-序号：${i} ## 生成名称: ${工具人们[i].name}"}]}`);return;
                 
-            };
-            if(消息== "攻击"){
-
-              let 眼前的工具人 = 获取眼前的假人实体(发起者, 16);
-                  眼前的工具人. addTag ( 攻击标识符);
-                  眼前的工具人.removeTag(挖掘标识符);
-                  眼前的工具人.removeTag(自动攻击标识符);
-                
-            };
-            if(消息== "自动攻击"){
-              
-              
-              let 眼前的工具人 = 获取眼前的假人实体(发起者, 16);
-                  眼前的工具人. addTag ( 自动攻击标识符);
-                  眼前的工具人.removeTag(挖掘标识符);
-                  眼前的工具人.removeTag(攻击标识符);
-
             };
             if(消息.startsWith("销毁")){
               
@@ -368,8 +355,8 @@ world.events.beforeChat.subscribe( M_event => {
                   眼前的工具人.teleport(new Location(x,y,z),眼前的工具人.dimension,0,0);                                    ;;"回家";;
 
               let name = 'xboy' + 眼前的工具人.name.replaceAll(" ","#") + '';
-                  眼前的工具人.runCommandAsync( `structure save ${name}_1 ${x-1} ${y+1} ${z-1} ${x+1} ${y+1} ${z-2} disk`);  ;;"保存原地形";;
-                  眼前的工具人.runCommandAsync( `structure save ${name}_0 ${x-3} ${y-2} ${z-3} ${x} ${y} ${z} disk`);
+                  眼前的工具人.runCommandAsync( `structure save ${name}_1 ${x-1} ${y+1} ${z-1} ${x+1} ${y+1} ${z-2} false memory true`);  ;;"保存原地形";;
+                  眼前的工具人.runCommandAsync( `structure save ${name}_0 ${x-3} ${y-2} ${z-3} ${x} ${y} ${z} false memory true`);
                   
               let 眼前的工具人dimension;
                   if(眼前的工具人.dimension == 主世界)眼前的工具人dimension=主世界;
@@ -382,53 +369,62 @@ world.events.beforeChat.subscribe( M_event => {
                     
 
             };
-            if(消息== "挖掘" && 0){
+            if(消息=="交换背包"){
+                  const s = 眼前的工具人.getComponent("inventory").container;                    ;;"眼前的假人实体背包";;
+                  const p = sender.getComponent("inventory").container;                                          ;;"你这个______的背包";;
+                  for(let i = sender.getComponent("inventory").container.size;i--;s.getItem(i)?p.getItem(i)?s.swapItems(i,i,p):s.transferItem(i,i,p):p.getItem(i)?p.transferItem(i,i,s):"这行代码，我再维护我是狗");
+            };
+            if(消息== "挖掘" && mojang.脑子){
               
-              
-              let 眼前的工具人 = 获取眼前的假人实体(发起者, 16);
-                  眼前的工具人.breakBlock(new BlockLocation(x,y-1,z))
-                  眼前的工具人. addTag ( 挖掘标识符);
+                  // 眼前的工具人.breakBlock(new BlockLocation(x,y-1,z))
+                  眼前的工具人. addTag  (挖掘标识符);
                   眼前的工具人.removeTag(攻击标识符);
                   眼前的工具人.removeTag(自动攻击标识符);
-
             };
-            if(消息== "扭头"){
-
-              let 眼前的工具人 = 获取眼前的假人实体(发起者, 16);
+            if(消息== "扭头"|| 消息== "转向"){
                   眼前的工具人.lookAtEntity(发起者);
-
-            };
-            if(消息== "停止"){
-
-              let 眼前的工具人 = 获取眼前的假人实体(发起者, 16);
-
-              眼前的工具人.removeTag(攻击标识符);
-              眼前的工具人.removeTag(自动攻击标识符);
-              眼前的工具人.removeTag(挖掘标识符);
             };
             if(消息== "移动"){
-
-
-            let 假人 = 获取眼前的假人实体(发起者,15);
-             ! (假人)
+             ! (眼前的工具人)
              ?  发起者.runCommandAsync(`tellraw @s {"rawtext":[{"text":"§e§l-光标方向，15格内没找到相关实体"}]}`)
-             :  假人.teleport(发起者.location,主世界,0,0);;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+             :  眼前的工具人.teleport(发起者.location,眼前的工具人.dimension,0,0);;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
              ;  
              ;  
              ;
             };
-            if(消息== "github"){
-              qrcode.generate("github.com/xBoyMinemc", function (str) {发起者.runCommandAsync("tell @s  https://github.com/xBoyMinemc 能不能扫上随缘\u000a"+str.replaceAll("#","\u000a").replaceAll("0","⬛").replaceAll("1","⬜"))})
-
+            const TagsManager =(xboy)=>(minemc)=>(need)=>(add)=>(remove)=>xboy===need?(add.length?add.forEach(t=>minemc.addTag(t)):0,remove.length?remove.forEach(t=>minemc.remove(t)):0):0;
+            const xboy = TagsManager(消息)(眼前的工具人)
+                  xboy("攻击")([攻击标识符])([自动攻击标识符,挖掘标识符])
+                  xboy("自动攻击")([自动攻击标识符])([攻击标识符,挖掘标识符])
+                  xboy("开始跳跃")([跳跃标识符])([])
+                  xboy("结束跳跃")([])([跳跃标识符])
+                  xboy("停止")([])([攻击标识符,自动攻击标识符,跳跃标识符,挖掘标识符])
+                  xboy("开摆")([])([攻击标识符,自动攻击标识符,跳跃标识符,挖掘标识符])
+            
+            if(消息== "开始使用"){
+                  眼前的工具人.useItemInSlot(0)
             };
-            if(消息== "帮助"){
-              ["###部分功能需要光标对准假人","创建","列表","攻击","自动攻击","销毁","销毁 0" ,"销毁 1","销毁 + 列表标号","扭头","停止","移动","#赠品：输入tps开/tps关","github","#赞助作者烂活？得了吧。。"].forEach((text)=>发起者.runCommandAsync(`tellraw @s {"rawtext":[{"text":"§e§l-${text}"}]}`))
+            if(消息== "停止使用"){
+                  眼前的工具人.stopUsingItem()
+            };
+            if(消息== "开始交互"){
+                  眼前的工具人.interact()
+            };
             
 
+
+            if(消息== "github"){
+              qrcode.generate("github.com/xBoyMinemc", function (str) {发起者.runCommandAsync("tell @s  https://github.com/xBoyMinemc 能不能扫上随缘\u000a"+str.replaceAll("#","\u000a").replaceAll("0","⬛").replaceAll("1","⬜"))})
+            };
+            if(消息== "帮助"){
+              ["###部分功能需要光标对准假人","创建","列表","扭头","停止","移动","攻击","自动攻击","交换背包","一般操作示例 '假人创建' '假人销毁' '假人交换背包'  ’假人github‘  ’假人help‘","销毁 + 空格 +列表标号","销毁示例","销毁","销毁 0" ,"销毁 1","#赠品：输入'tps开' 或 'tps关'","github","#赞助作者烂活？得了吧。。"].forEach((text)=>发起者.runCommandAsync(`tellraw @s {"rawtext":[{"text":"§e§l-${text}"}]}`))
+            };
+            if(消息== "help"){
+              ["这里是一些技术解释","如果出现莫名其妙的Refer什么什么错误，可能是1.19.40+的垃圾特性，重启即可，有概率因为/reload或进入游戏而出现","1.19.40版本的假人销毁，并不是真正意义上的销毁，可以定期/reload而真正释放","积累过多假人可能会增加不可预测的bug被触发的概率","文件充满汉语是整活","现在依旧是汉语是因为整活把源文件整丢了"].forEach((text)=>发起者.runCommandAsync(`tellraw @s {"rawtext":[{"text":"§e§l-${text}"}]}`))
             };
       M_event.cancel=ture;
        return;
-    } catch (err) {//${err}
+    } catch (err) {
       主世界.runCommandAsync(`me 【假人ERROR】${unescape("\u000a")}* 你触发了一个错误   ${unescape("\u000a")}* 考虑你没对准，歪了+${err}`)
     }
    
