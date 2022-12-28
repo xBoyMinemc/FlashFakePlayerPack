5869dd75743e1613c53cc18210777f28bc116766
xBoyMinemc <86919167+xBoyMinemc@users.noreply.github.com>
2022-12-28 06:36:24 +0000

    Add files via upload

--- scripts/lib/xboyEvents/entityDeadByHurt.js
+++ scripts/lib/xboyEvents/entityDeadByHurt.js
@@ -27,9 +27,9 @@ world.events.entityHurt.subscribe(event=>
 )
 
 
-const test = (event)=>event.hurtEntity.dimension.runCommandAsync("me "+(event.hurtEntity.name??event.hurtEntity.nameTag??event.hurtEntity.typeId)+"#x=>"+event.hurtEntity.location.x+"#y=>"+event.hurtEntity.location.y+"#z=>"+event.hurtEntity.location.z +"被杀死"+"\u000a触发自事件world.events.entityDeadByHurt" )
+// const test = (event)=>event.hurtEntity.dimension.runCommandAsync("me "+(event.hurtEntity.name??event.hurtEntity.nameTag??event.hurtEntity.typeId)+"#x=>"+event.hurtEntity.location.x+"#y=>"+event.hurtEntity.location.y+"#z=>"+event.hurtEntity.location.z +"被杀死"+"\u000a触发自事件world.events.entityDeadByHurt" )
 
-entityDeadByHurt.subscribe(test)
+// entityDeadByHurt.subscribe(test)
 
 
 
