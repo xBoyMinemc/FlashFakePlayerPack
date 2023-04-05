de524fdb454bf1a5321ecdd540374385272e9b0f
xBoyMinemc <86919167+xBoyMinemc@users.noreply.github.com>
2023-04-05 17:04:12 +0000

    清明时雨 代码整理 祭奠过去

--- scripts/tick.js
+++
@@ -1,5 +0,0 @@
-import { system } from "@minecraft/server";
-import EventSignal from "./EventSignal";
-const tick = new EventSignal();
-world.events.tick = tick;
-system.runInterval(() => world.events.tick.trigger(null), 1);
