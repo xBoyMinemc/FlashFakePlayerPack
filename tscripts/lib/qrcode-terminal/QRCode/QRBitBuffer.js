9ef872f02bd9d67741b4c1fe8c99013530fd7fa3
xBoyMinemc <86919167+xBoyMinemc@users.noreply.github.com>
2023-04-06 03:21:42 +0000

    代码整理-B 准备模块化

---
+++ tscripts/lib/qrcode-terminal/QRCode/QRBitBuffer.js
@@ -0,0 +1,37 @@
+export default class QRBitBuffer {
+	constructor() {
+		this.buffer = [];
+		this.length = 0;
+	}
+
+	get(index) {
+		var bufIndex = Math.floor(index / 8);
+		return ( (this.buffer[bufIndex] >>> (7 - index % 8) ) & 1) == 1;
+	}
+	
+	put(num, length){
+		for (var i = 0; i < length; i++) {
+			this.putBit( ( (num >>> (length - i - 1) ) & 1) == 1);
+		}
+	}
+	
+	getLengthInBits() {
+		return this.length;
+	}
+	
+	putBit(bit) {
+	
+		var bufIndex = Math.floor(this.length / 8);
+		if (this.buffer.length <= bufIndex) {
+			this.buffer.push(0);
+		}
+	
+		if (bit) {
+			this.buffer[bufIndex] |= (0x80 >>> (this.length % 8) );
+		}
+	
+		this.length++;
+	}
+}
+
+
