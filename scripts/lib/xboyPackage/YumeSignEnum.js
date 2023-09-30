018fb9dd5bfa85d4137e77b5ada6a8b4ff92f6f0
xBoyMinemc <86919167+xBoyMinemc@users.noreply.github.com>
2023-09-30 14:42:00 +0800

    sign enum

---
+++ scripts/lib/xboyPackage/YumeSignEnum.js
@@ -0,0 +1,7 @@
+export var SIGN;
+(function (SIGN) {
+    SIGN["AUTO_RESPAWN_SIGN"] = "AUTO_RESPAWN_SIGN";
+    SIGN["YUME_SIM_SIGN"] = "#yumeSimSign#";
+})(SIGN || (SIGN = {}));
+export default SIGN;
+export const AUTO_RESPAWN_SIGN = "AUTO_RESPAWN_SIGN";
