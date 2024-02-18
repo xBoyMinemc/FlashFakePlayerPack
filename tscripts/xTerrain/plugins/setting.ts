94d723d1bddaecb01f90783518fb3ffe9e1fb2c1
xBoyMinemc <86919167+xBoyMinemc@users.noreply.github.com>
2024-02-18 16:30:51 +0800

    balabal

--- tscripts/xTerrain/plugins/setting.ts
+++ tscripts/xTerrain/plugins/setting.ts
@@ -11,23 +11,22 @@ const commandRegistry: CommandRegistry = new CommandRegistry()
 commandRegistry.registerAlias('假人重置编号','假人重置序号')
 commandRegistry.registerAlias('假人编号重置','假人重置序号')
 commandRegistry.registerAlias('假人序号重置','假人重置序号')
-commandRegistry.registerAlias('假人序号重置','假人重置序号')
 commandRegistry.registerCommand('假人重置序号', ({ entity }) => {
 
-    const SetPID = (PID:number=0)=>{
+    const SetPID = (PID:number=1)=>{
         const __FlashPlayer__ = <ScoreboardObjective>ScoreBase.GetObject('##FlashPlayer##')
 
         const value = ScoreBase.GetPoints(__FlashPlayer__,'##currentPID')
 
-        __FlashPlayer__.setScore('##currentPID',PID)
+              __FlashPlayer__.setScore('##currentPID',PID)
 
         return value
     }
-    const PID = SetPID(0)
-    entity.sendMessage('重置成功，重置前为'+PID)
+    const PID = SetPID(1)
+    entity?.sendMessage('重置成功，重置前为'+PID)
 
 
-    entity.sendMessage('以前是以前✋ ，现在是现在✋ ，你要是一直拿以前当作现在✋ ，哥们，你怎么不拿你开新档的时候对比')
+    entity?.sendMessage('以前是以前✋ ，现在是现在✋ ，你要是一直拿以前当作现在✋ ，哥们，你怎么不拿你开新档的时候对比')
 })
 
 world.afterEvents.chatSend.subscribe(({message, sender})=>{
