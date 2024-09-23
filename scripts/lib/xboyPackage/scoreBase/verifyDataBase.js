816c1ca7f64774de22b0b85fc2dd11ec7ac407b1
xBoyMinemc <86919167+xBoyMinemc@users.noreply.github.com>
2024-09-24 01:48:17 +0800

    nothing

--- scripts/lib/xboyPackage/scoreBase/verifyDataBase.js
+++ scripts/lib/xboyPackage/scoreBase/verifyDataBase.js
@@ -4,11 +4,7 @@ let ScoreBaseSnapshot = ScoreBase.GetObject();
 const checkScoreObjectExist = (ScoreObjectName) => !!Array.from(ScoreBaseSnapshot).find(ScoreObject => ScoreObjectName === ScoreObject.id);
 const verify = function () {
     ScoreBaseSnapshot = ScoreBase.GetObject();
-    ['##FlashPlayer##'].forEach(_ => checkScoreObjectExist(_)
-        ? console.warn('[模拟玩家] 计分板对象数据存在==>', _)
-        : console.warn('[模拟玩家] 计分板对象数据不存在但已创建==>', ScoreBase.NewObjectAsync(_).displayName));
-    world.scoreboard.getObjective('##FlashPlayer##').hasParticipant('##currentPID')
-        ? console.warn('[模拟玩家] 计分板键值数据存在==>', '##currentPID')
-        : (ScoreBase.SetPoints('##FlashPlayer##', '##currentPID', 1), console.warn('[模拟玩家] 数据不存在但已创建==>', '##FlashPlayer## * currentUID'));
+    ['##FlashPlayer##'].forEach(_ => checkScoreObjectExist(_) || ScoreBase.NewObjectAsync(_).displayName);
+    world.scoreboard.getObjective('##FlashPlayer##').hasParticipant('##currentPID') || ScoreBase.SetPoints('##FlashPlayer##', '##currentPID', 1);
 };
 export default verify;
--- tscripts/lib/xboyPackage/scoreBase/verifyDataBase.ts
+++ tscripts/lib/xboyPackage/scoreBase/verifyDataBase.ts
@@ -8,14 +8,11 @@ const checkScoreObjectExist = (ScoreObjectName : string) : boolean =>  !!Array.f
 const verify = function(){
     ScoreBaseSnapshot = <ScoreboardObjective[]>ScoreBase.GetObject();
     ['##FlashPlayer##'].forEach(_=>
-        checkScoreObjectExist(_)
-        ? console.warn('[模拟玩家] 计分板对象数据存在==>',_)
-        : console.warn('[模拟玩家] 计分板对象数据不存在但已创建==>',ScoreBase.NewObjectAsync(_).displayName)
+        checkScoreObjectExist(_) || ScoreBase.NewObjectAsync(_).displayName
     )
 
-        ; world.scoreboard.getObjective('##FlashPlayer##').hasParticipant('##currentPID')
-        ? console.warn( '[模拟玩家] 计分板键值数据存在==>','##currentPID')
-        : (ScoreBase.SetPoints('##FlashPlayer##','##currentPID',1),console.warn('[模拟玩家] 数据不存在但已创建==>','##FlashPlayer## * currentUID'))
+    world.scoreboard.getObjective('##FlashPlayer##').hasParticipant('##currentPID') || ScoreBase.SetPoints('##FlashPlayer##','##currentPID',1)
+
 
 }
 
