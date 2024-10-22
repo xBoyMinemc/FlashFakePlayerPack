fef5d0be87dbbdd50766ecc742183943776100cf
xBoyMinemc <86919167+xBoyMinemc@users.noreply.github.com>
2024-10-22 18:48:34 +0800

    修复虚空索敌的报错

--- tscripts/xTerrain/plugins/killedBySimPlayer.ts
+++ tscripts/xTerrain/plugins/killedBySimPlayer.ts
@@ -1,13 +1,14 @@
 import entityDeadByHurt from '../../lib/xboyEvents/entityDeadByHurt'
 import {SimulatedPlayerEnum} from '../main'
 import {SimulatedPlayer} from '@minecraft/server-gametest'
+import {world} from "@minecraft/server";
 
 entityDeadByHurt.subscribe(({damageSource,hurtEntity})=>{
     if(hurtEntity.typeId !== 'minecraft:player')return
     if(!damageSource)return
 
     if(SimulatedPlayerEnum[hurtEntity.id])
-        return damageSource.damagingEntity['sendMessage']('玩不起，就别玩')
+        return damageSource ?? damageSource?.damagingEntity['sendMessage']('玩不起，就别玩')
 
     const PID = SimulatedPlayerEnum[damageSource.damagingEntity.id]
     if(!PID)return
