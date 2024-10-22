4c58ccc314265ff5cbc6f281c7bd7fb6afe1f7ce
xBoyMinemc <86919167+xBoyMinemc@users.noreply.github.com>
2024-10-22 19:48:07 +0800

    支持1.21.30与1.21.20

--- packer.js
+++ packer.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 const archiver = require('archiver');
 
 
-const pkNew = '柔和修复生物生成-随机刻-时间'
-const mcVersion = [1,21,31];
-const pkVersion = 1
+const pkNew = '多版本通配测试-构建于1.21.30-支持1.21.2x-可能支持更新版本'
+const mcVersion = [1,21,20];
+const pkVersion = 10
 
 
 
