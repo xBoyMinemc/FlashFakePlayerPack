148510fd622fa78909cc3d2a4a8134d23d8ac6c4
PuppyOne <zheng_xz@qq.com>
2025-01-25 17:31:51 +0800

    chore(showCommandsList): 删除调试用日志代码

--- tscripts/xTerrain/plugins/showCommandsList.ts
+++ tscripts/xTerrain/plugins/showCommandsList.ts
@@ -2,8 +2,6 @@ import { Command, commandManager } from "../../lib/yumeCommand/CommandRegistry";
 
 const listCommands = new Command();
 listCommands.register(({ entity }) => {
-    console.log(1);
-
     entity?.sendMessage(commandManager.listRegisteredPrefixes().join('\n'));
 });
 commandManager.registerCommand(['showshowway', '假人命令列表'], listCommands);
