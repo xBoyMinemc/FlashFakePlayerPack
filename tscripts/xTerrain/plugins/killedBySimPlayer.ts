abd98c8758ac0141540433e3f72b10763f86c481
PuppyOne <zheng_xz@qq.com>
2025-02-03 14:53:20 +0800

    refactor(killedBySimPlayer): 使用参数解构
    在 `entityDeadByHurt` 事件层面已过滤 `event.damageSource.cause === EntityDamageCause.entityAttack`，所以 `damageSource.damagingEntity` 一定存在

--- tscripts/xTerrain/plugins/killedBySimPlayer.ts
+++ tscripts/xTerrain/plugins/killedBySimPlayer.ts
@@ -3,13 +3,13 @@ import entityDeadByHurt from '../../lib/xboyEvents/entityDeadByHurt'
 import {simulatedPlayers} from '../main'
 import {SimulatedPlayer} from '@minecraft/server-gametest'
 
-entityDeadByHurt.subscribe(({ damageSource, deadEntity }) => {
-    if (deadEntity.typeId !== 'minecraft:player' || damageSource.damagingEntity.typeId !== 'minecraft:player') return;
+entityDeadByHurt.subscribe(({ damageSource: { damagingEntity }, deadEntity }) => {
+    if (deadEntity.typeId !== 'minecraft:player' || damagingEntity.typeId !== 'minecraft:player') return;
 
     if (simulatedPlayers[deadEntity.id])
-        return (<Player>damageSource.damagingEntity).sendMessage('玩不起，就别玩');
+        return (<Player>damagingEntity).sendMessage('玩不起，就别玩');
 
-    const PID = simulatedPlayers[damageSource.damagingEntity.id];
+    const PID = simulatedPlayers[damagingEntity.id];
     if (!PID) return;
 
     const SimPlayer = <SimulatedPlayer>simulatedPlayers[PID];
