a47b25c1e0560cd092746ae36cf356210aa3ecf7
PuppyOne <zheng_xz@qq.com>
2025-02-03 14:47:07 +0800

    style(killedBySimPlayer): 稍微格式化代码

--- tscripts/xTerrain/plugins/killedBySimPlayer.ts
+++ tscripts/xTerrain/plugins/killedBySimPlayer.ts
@@ -3,18 +3,18 @@ import entityDeadByHurt from '../../lib/xboyEvents/entityDeadByHurt'
 import {simulatedPlayers} from '../main'
 import {SimulatedPlayer} from '@minecraft/server-gametest'
 
-entityDeadByHurt.subscribe(({damageSource,deadEntity})=>{
-    if (deadEntity.typeId !== 'minecraft:player' || damageSource.damagingEntity.typeId !== 'minecraft:player') return
+entityDeadByHurt.subscribe(({ damageSource, deadEntity }) => {
+    if (deadEntity.typeId !== 'minecraft:player' || damageSource.damagingEntity.typeId !== 'minecraft:player') return;
 
-    if(simulatedPlayers[deadEntity.id])
-        return (<Player>damageSource.damagingEntity).sendMessage('玩不起，就别玩')
+    if (simulatedPlayers[deadEntity.id])
+        return (<Player>damageSource.damagingEntity).sendMessage('玩不起，就别玩');
 
-    const PID = simulatedPlayers[damageSource.damagingEntity.id]
-    if(!PID)return
+    const PID = simulatedPlayers[damageSource.damagingEntity.id];
+    if (!PID) return;
 
-    const SimPlayer = <SimulatedPlayer>simulatedPlayers[PID]
+    const SimPlayer = <SimulatedPlayer>simulatedPlayers[PID];
 
-    if(!SimPlayer)return
+    if (!SimPlayer) return;
 
-    (<Player>deadEntity).sendMessage('菜，就多练')
-})
+    (<Player>deadEntity).sendMessage('菜，就多练');
+});
