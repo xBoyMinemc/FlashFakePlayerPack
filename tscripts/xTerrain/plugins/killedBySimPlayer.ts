7c9aa73a6a5d616ec21732b0f3370be6458bcf94
PuppyOne <zheng_xz@qq.com>
2025-02-18 17:09:49 +0800

    refactor(killedBySimPlayer): 移除不再使用的导入

--- tscripts/xTerrain/plugins/killedBySimPlayer.ts
+++ tscripts/xTerrain/plugins/killedBySimPlayer.ts
@@ -1,7 +1,6 @@
 import type { Player } from '@minecraft/server';
 import entityDeadByHurt from '../../lib/xboyEvents/entityDeadByHurt'
 import {simulatedPlayers} from '../main'
-import {SimulatedPlayer} from '@minecraft/server-gametest'
 
 entityDeadByHurt.subscribe(({ damageSource: { damagingEntity }, deadEntity }) => {
     if (deadEntity.typeId !== 'minecraft:player' || damagingEntity.typeId !== 'minecraft:player') return;
