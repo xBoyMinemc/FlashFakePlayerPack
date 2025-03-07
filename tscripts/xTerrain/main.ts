f3b78cc4c1bbe10cb9044bd90e75c47f748bcb7f
PuppyOne <zheng_xz@qq.com>
2025-03-07 22:06:49 +0800

    revert: 回退测试用更改
    Reverts
    
    - e37439235a0784f86dd3dc38c7ca98c2ad2c8ace
    - 7427a5aa81161057db5249c3b23da9355f2bec75
    - 26b5e2c969b33e64dd087da0ebdcd9514a59324a
    
    防止上游仓库触发 Release

--- tscripts/xTerrain/main.ts
+++ tscripts/xTerrain/main.ts
@@ -137,7 +137,7 @@ register('我是云梦', '假人', (test:Test) => {
 let say = false
 playerMove.subscribe(()=>{
     if (say) return
-    say  = true
+    say = true
     world.sendMessage('[模拟玩家] 初始化完成，输入“假人创建”或“ffpp”')
 })
 
@@ -179,5 +179,4 @@ world.beforeEvents.chatSend.subscribe(({message, sender}) => {
 });
 
 export { spawnSimulatedPlayer,spawnSimulatedPlayerByNameTag,testWorldLocation,GetPID }
-
 
