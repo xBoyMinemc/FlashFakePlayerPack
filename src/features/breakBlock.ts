f4fd5b3d3b398a2f70bfde9cbd2c786b48bef4ce
PuppyOne <zheng_xz@qq.com>
2025-04-16 21:44:27 +0800

    refactor(breakBlock): 优化 API 使用 (#28)
    - 实现上主要更改了方块位置相对坐标的转换，改用了游戏测试框架原生 API 365ccb23bef0dc7cac46579385723a280aad839e
    - 清理了部分冗余代码和不再需要的注释、导入
    - 优化了代码格式

--- src/features/breakBlock.ts
+++ src/features/breakBlock.ts
@@ -1,9 +1,8 @@
-import type { SimulatedPlayer } from '@minecraft/server-gametest'
-import {Dimension, Player, Vector3} from '@minecraft/server'
+import type { SimulatedPlayer } from '@minecraft/server-gametest';
 
 import { Command, commandManager } from '../core/command';
-import { getSimPlayer } from '../core/queries/Util'
-import { world, system } from "@minecraft/server"
+import { getSimPlayer } from '../core/queries/Util';
+import { world, system } from "@minecraft/server";
 import SIGN from "../constants/YumeSignEnum";
 import { testManager } from '../main';
 
@@ -14,40 +13,28 @@ breakBlockCommand.register(({ args }) => args.length === 0, ({ entity, isEntity 
         return;
     }
 
-    const SimPlayer: SimulatedPlayer = getSimPlayer.fromView(entity)
+    const SimPlayer: SimulatedPlayer = getSimPlayer.fromView(entity);
     if (!SimPlayer) {
         entity.sendMessage('§e§l-面前不存在模拟玩家');
         return;
-    } 
+    }
 
-    SimPlayer.addTag(SIGN.AUTO_BREAKBLOCK_SIGN)
-
-    // console.error('[假人]内置插件'+'假人挖掘'+'执行成功')
-
+    SimPlayer.addTag(SIGN.AUTO_BREAKBLOCK_SIGN);
 });
 commandManager.registerCommand(['假人挖掘', '假人摧毁'], breakBlockCommand);
 
-const Vector_subtract = ({x,y,z}:Vector3, {x:u,y:v,z:w}:Vector3)=>({x:x-u,y:y-v,z:z-w})
-const Vector_addition = ({x,y,z}:Vector3, {x:u,y:v,z:w}:Vector3)=>({x:x+u,y:y+v,z:z+w})
-const Vector_multiplication_dot = ({x,y,z}:Vector3, u:number)=>({x:x*u,y:y*u,z:z*u})
-
-// type awa = 'awa'
-
 // task
-const breaks = (/*awa:awa='awa'*/)=>
-    world.getPlayers({tags:[SIGN.AUTO_BREAKBLOCK_SIGN]}).forEach( async SimPlayer => {
-        // getHeadLocation
-        // getViewDirection
-        // 这是一会要用到的妙妙工具
-        const man = <SimulatedPlayer><unknown>SimPlayer
-        const block =  man.getBlockFromViewDirection({maxDistance:6})?.block
-        if (!block) return
+const breaks = () =>
+    world.getPlayers({ tags: [SIGN.AUTO_BREAKBLOCK_SIGN] }).forEach(async SimPlayer => {
+        const man = <SimulatedPlayer><unknown>SimPlayer;
+        const block = man.getBlockFromViewDirection({ maxDistance: 6 })?.block;
+        if (!block) return;
 
-        if (block.isValid && !block.isLiquid && !block.isAir){
-            man.breakBlock(Vector_subtract(block, testManager.testLocation))
+        if (block.isValid && !block.isLiquid && !block.isAir) {
+            man.breakBlock(testManager.test.relativeBlockLocation(block));
         }
-    })
+    });
 
-system.runInterval(breaks,0) // 2 + 0 = 0
+system.runInterval(breaks, 0);
 
 // console.error('[假人]内置插件'+commandName+'加载成功')
