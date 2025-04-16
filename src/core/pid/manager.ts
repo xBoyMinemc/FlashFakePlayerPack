96ff0b5017072504aec381443e327d3ac4061cde
PuppyOne <zheng_xz@qq.com>
2025-04-16 11:00:16 +0800

    refactor(core/pid): 减少硬编码字符串 (#19)
    使用私有只读属性替代直接使用字符串字面量

--- src/core/pid/manager.ts
+++ src/core/pid/manager.ts
@@ -2,24 +2,27 @@ import { world } from '@minecraft/server';
 import type { PID } from './types';
 
 export class PIDManager {
+    private readonly scoreboardName = '##FlashPlayer##';
+    private readonly scoreName = '##currentPID';
+
     constructor(private readonly initialValue: PID = 0 as PID) {}
 
     initialize(): void {
-        if (!world.scoreboard.getObjective('##FlashPlayer##'))
-            world.scoreboard.addObjective('##FlashPlayer##');
+        if (!world.scoreboard.getObjective(this.scoreboardName))
+            world.scoreboard.addObjective(this.scoreboardName);
 
-        if (!world.scoreboard.getObjective('##FlashPlayer##').hasParticipant('##currentPID'))
-            world.scoreboard.getObjective('##FlashPlayer##').setScore('##currentPID', this.initialValue);
+        if (!world.scoreboard.getObjective(this.scoreboardName).hasParticipant(this.scoreName))
+            world.scoreboard.getObjective(this.scoreboardName).setScore(this.scoreName, this.initialValue);
     }
 
     next(): PID {
-        const pid = world.scoreboard.getObjective('##FlashPlayer##').addScore('##currentPID', 1) as PID;
+        const pid = world.scoreboard.getObjective(this.scoreboardName).addScore(this.scoreName, 1) as PID;
         return pid;
     }
 
     reset(): PID {
-        const currentPID = world.scoreboard.getObjective('##FlashPlayer##').getScore('##currentPID') as PID;
-        world.scoreboard.getObjective('##FlashPlayer##').setScore('##currentPID', this.initialValue);
+        const currentPID = world.scoreboard.getObjective(this.scoreboardName).getScore(this.scoreName) as PID;
+        world.scoreboard.getObjective(this.scoreboardName).setScore(this.scoreName, this.initialValue);
         return currentPID;
     }
 }
