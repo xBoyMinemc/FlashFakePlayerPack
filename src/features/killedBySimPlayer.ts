40b4ad02edfb2ca2ce28d9dc29a1fcb6e18e73bd
PuppyOne <zheng_xz@qq.com>
2025-04-17 17:12:20 +0800

    refactor: 移除 `entityDeadByHurt` 事件 对应逻辑上移至 `killedBySimPlayer` 处理 (#33)
    事件需求复杂度低，直接使用原生 API 事件

--- src/features/killedBySimPlayer.ts
+++ src/features/killedBySimPlayer.ts
@@ -1,8 +1,9 @@
-import type { Player } from '@minecraft/server';
-import entityDeadByHurt from '../lib/xboyEvents/entityDeadByHurt'
+import { EntityDamageCause, world, type Player } from '@minecraft/server';
 import { simulatedPlayerManager } from '../main';
 
-entityDeadByHurt.subscribe(({ damageSource: { damagingEntity }, deadEntity }) => {
+world.afterEvents.entityDie.subscribe(({ damageSource: { cause, damagingEntity }, deadEntity }) => {
+    if (cause !== EntityDamageCause.entityAttack) return;
+
     if (deadEntity.typeId !== 'minecraft:player' || damagingEntity.typeId !== 'minecraft:player') return;
 
     if (!(simulatedPlayerManager.has(deadEntity) || simulatedPlayerManager.has(damagingEntity))) return;
--- src/lib/xboyEvents/index.ts
+++ src/lib/xboyEvents/index.ts
@@ -1,11 +1,9 @@
-import entityDeadByHurt from "./entityDeadByHurt"
 import { fishingHookSpawned, fishingHookDespawned } from "./fishingHookSpawned"
 import reloadFromCmd from "./reloadFromCmd"
 import { playerReady } from "./ready";
 
 const events = {
     playerMove: playerReady,
-    entityDeadByHurt: entityDeadByHurt,
     fishingHookSpawned: fishingHookSpawned,
     fishingHookDespawned: fishingHookDespawned
 }
