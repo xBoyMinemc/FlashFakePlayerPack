46638642ea724dfe0b4076c7e92b84ef4cfff474
PuppyOne <zheng_xz@qq.com>
2025-04-20 21:26:24 +0800

    docs(core/command/command-parser): 移动 TSDoc 至 `command-parser.ts` (#60)

--- src/core/command/command-parser.ts
+++ src/core/command/command-parser.ts
@@ -2,6 +2,19 @@ import { InvalidInputError } from "./errors";
 
 const regex = /"([^"]*)"|'([^']*)'|(\S+)/g; // 正则匹配所有单词或引号内的文本
 
+/**
+ * 解析命令字符串。
+ *
+ * @param input - 需要解析的命令输入字符串。
+ * @returns 返回一个包含命令前缀和参数数组的对象。
+ * @throws {InvalidInputError} 当输入无效时抛出异常。
+ *
+ * @example
+ * ```typescript
+ * console.log(parseCommandString("say player 'Hello World'"));
+ * // { prefix: 'say', args: [ 'player', 'Hello World' ] }
+ * ```
+ */
 export function parseCommandString(input: string): { prefix: string; args: string[]; } {
     const parts = [];
     let match: RegExpMatchArray | null;
