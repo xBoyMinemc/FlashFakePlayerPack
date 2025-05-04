d44b35889c4840e8aac620fc329be49c65e11244
PuppyOne <zheng_xz@qq.com>
2025-05-04 14:58:54 +0800

    refactor: 上移 `commands/*/` 目录 (#83)
    上移至 `src/` 下 由于并非单纯的命令注册实现

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,4 +1,28 @@
-export * from './commands';
 export * from './task';
 export * from './gui';
 export * from './killedBySimPlayer';
+
+export * from './command-triggers';
+
+export * from './meta/help';
+export * from './meta/list-simulated-players';
+export * from './meta/location';
+export * from './meta/rename';
+export * from './meta/reset-pid';
+export * from './meta/list-commands';
+export * from './meta/time';
+
+export * from './lifecycle/spawn';
+export * from './lifecycle/disconnect';
+export * from './lifecycle/respawn';
+
+export * from './inventory/equipment-swap';
+export * from './inventory/inventory-swap';
+export * from './inventory/mainhand-swap';
+export * from './inventory/offhand-swap';
+export * from './inventory/recycle';
+
+export * from './behaviors/behaviors';
+export * from './behaviors/break-block';
+
+export * from './tps/indicator'
