cf9d86ffe79d5045bf21c60334d7ad5911e0ab59
kzyqq00-Player <kzyqq00-Player@outlook.com>
2025-08-15 11:49:11 +0800

    fix: listenChatMessage的回调函数里return不影响上级函数

--- tscripts/xTerrain/plugins/clearSimulatedPlayers.ts
+++ tscripts/xTerrain/plugins/clearSimulatedPlayers.ts
@@ -1,11 +1,25 @@
 import {Command, commandManager} from "../../lib/yumeCommand/CommandRegistry";
 import {listenChatMessage, simulatedPlayers, unlistenChatMessage} from "../main";
+import {system} from "@minecraft/server";
 
 const cmd = new Command();
 cmd.register(inf => {
+    let reply = false;
+    let replied = false;
+    const prom = new Promise<void>((resolve, reject) => {
+        system.runInterval(() => {
+            if (replied) {
+                reply
+                    ? resolve(undefined)
+                    : reject();
+            }
+        })
+    })
+
     const listener = ({message}) => {
-        if (message.toLowerCase() === "n" || message.toLowerCase() !== "y") {
-            return;
+        replied = true;
+        if (!(message.toLowerCase() === "n" || message.toLowerCase() !== "y")) {
+            reply = true;
         }
     };
 
@@ -14,11 +28,13 @@ cmd.register(inf => {
         inf.entity.sendMessage('确定删除所有假人吗？(请在聊天框回复y/N)');
     }
 
-    for (let simulatedPlayersKey in simulatedPlayers) {
-        simulatedPlayers.PID.disconnect();
-    }
-
-    unlistenChatMessage(listener);
+    prom.then(() => {
+        for (let _x in simulatedPlayers) {
+            simulatedPlayers.PID.disconnect();
+        }
+    }).finally(() => {
+        unlistenChatMessage(listener);
+    });
 });
 
 commandManager.registerCommand(['ras', '删除所有假人'], cmd);
