2071d1063e042095b970a4d1406f05c788ac865a
kzyqq00-Player <kzyqq00-Player@outlook.com>
2025-08-16 20:23:10 +0800

    refactor: 优化`tpsShower`

--- tscripts/lib/xboyTools/tpsShower.ts
+++ tscripts/lib/xboyTools/tpsShower.ts
@@ -1,10 +1,20 @@
 import { world } from "@minecraft/server"
+import { Command, commandManager } from "../yumeCommand/CommandRegistry";
 
-world.afterEvents.chatSend.subscribe(({message,sender})=>{
-    if(message === "tps开")sender.addTag("tps")
-    if(message === "tps关")sender.removeTag("tps")
-})
-
+const cmd = [new Command(), new Command()];
+function onNotEntity() {
+    console.warn('[模拟玩家] 命令只能由实体执行');
+}
+cmd[0].register(({entity})=>{
+    if(!entity) onNotEntity();
+    entity.addTag("tps");
+});
+cmd[1].register(({entity})=>{
+    if(!entity) onNotEntity();
+    entity.removeTag("tps");
+});
+commandManager.registerCommand(['tpson', 'tps开'], cmd[0]);
+commandManager.registerCommand(['tpsoff', 'tps关'], cmd[1]);
 
 let tps = 0,
     currentTime =  new Date().getSeconds()
@@ -12,13 +22,17 @@ let tps = 0,
 const tpsShower = ()=>{
     ++tps
 
-    if(new Date().getSeconds() === currentTime)return 0
+    if(new Date().getSeconds() === currentTime)return
     currentTime =  new Date().getSeconds()
     // overworld.runCommandAsync(`title @a[tag=tps] actionbar §e§lTPS:§3${tps}§0#§4PPT:§e${mspta}-${msptb}§0#§4${msptArray.join(",")}`)
 
     // overworld.runCommandAsync(`title @a[tag=tps] actionbar §e§lTPS:§3${tps}`)
     // no command,on $player.onScreenDisplay.setActionBar(`§e§lTPS:§3${tps}`)$
-    world.getAllPlayers().forEach(player=>player.hasTag("tps")?player.onScreenDisplay.setActionBar(`§e§lTPS:§3${tps}`):0)
+    world.getAllPlayers().forEach(player => {
+        if (player.hasTag("tps")) {
+            player.onScreenDisplay.setActionBar(`§e§lTPS:§3${tps}`)
+        }
+    })
     tps = 0
 
 }
