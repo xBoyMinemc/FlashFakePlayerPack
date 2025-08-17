58968c7b4327c6e280310f16587a7a09139c6750
xBoyMinemc <86919167+xBoyMinemc@users.noreply.github.com>
2025-08-17 22:03:36 +0800

    server-gametest模块在吗？

--- tscripts/main/preload.ts
+++ tscripts/main/preload.ts
@@ -1,7 +1,23 @@
 import { world } from "@minecraft/server";
 import "../lib/xboyEvents/preload.js"
 
-world.afterEvents.worldLoad.subscribe(() => import("./main.js"))
+let gametestAready = false
+async function gametestWhere() {
+    try {
+        const { registerAsync } = await import('@minecraft/server-gametest')
+        gametestAready = true
+    } catch (error) {
+        gametestAready = false
+    }
+}
+
+
+world.afterEvents.worldLoad.subscribe(async () => {
+    await gametestWhere()
+    if(!gametestAready)
+        world.sendMessage("【ERROR】@minecraft/server-gametest 未加载，你可能在Realm或aternos，aternos为了避免资源滥用，禁用了gametest。因为它可以伪造玩家在线数据。")
+    import("./main.js")
+})
 // 2025-5-8  1.21.80 又出问题 通过延迟到 worldLoad 事件解决
 //这里是为了解决一个莫名其妙的1.21.50触发的bug，当包加载的时候，加载world的部分方法会崩。而异步可以解决
 
